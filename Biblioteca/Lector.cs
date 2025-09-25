91ac43ae478de948dd6ff4a1b3b036134ce11ba5
Agustin Lopez <aalopezba@gmail.com>
2025-09-25 20:53:38 -0300

    feat: add Nombre to lector class

--- Biblioteca/Lector.cs
+++ Biblioteca/Lector.cs
@@ -5,14 +5,16 @@ namespace Biblioteca
 {
     internal class Lector
     {
+        public string Nombre { get; set; }
         public string Dni
         {
             get; set;
         }
         public List<Libro> librosPrestados { get; set; }
 
-        public Lector(string dni)
+        public Lector(string nombre, string dni)
         {
+            Nombre = nombre;
             Dni = dni;
             librosPrestados = new List<Libro>();
         }
