4b6d102cb41ff7be88873c3cbd4bf28885f42d19
Agustin Lopez <aalopezba@gmail.com>
2025-09-25 20:25:02 -0300

    init commit

---
+++ Biblioteca/Biblioteca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    internal class Biblioteca
+    {
+        private List<Libro> libros;
+
+        public Biblioteca()
+        {
+            this.libros = new List<Libro>();
+        }
+    }
+}
---
+++ Biblioteca/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblioteca = new Biblioteca();
+            cargarLibros(biblioteca, 10);
+            cargarLibros(biblioteca, 2);
+            biblioteca.listarLibros();
+            biblioteca.eliminarLibro("Libro5");
+            biblioteca.listarLibros();
+            void cargarLibros(Biblioteca biblioteca, int cantidad)
+            {
+                bool pude;
+                for (int i = 1; i <= cantidad; i++)
+                {
+                    pude = biblioteca.agregarLibro("Libro" + i, "Autor" + i, "Editorial" + i);
+                    if (pude)
+                        Console.WriteLine("Libro" + i + " agregado correctamente.");
+                    else
+                        Console.WriteLine("Libro" + i + " Ya existe en la biblioteca.");
+                }
+            }
+        }
+    }
+}
