474167df83488f04dca59e834dce645033fa4640
Agustin Lopez <aalopezba@gmail.com>
2025-09-25 21:53:31 -0300

    feat: add altaLector method

--- Biblioteca/Biblioteca.cs
+++ Biblioteca/Biblioteca.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Biblioteca
 {
     internal class Biblioteca
     {
         private List<Libro> libros;
+        private List<Lector> lectores;
 
         public Biblioteca()
         {
             this.libros = new List<Libro>();
+            this.lectores = new List<Lector>();
+        }
+
+        public bool altaLector(string dni, string nombre)
+        {
+
+            if (lectores.Any(l => l.Dni == dni))
+            {
+                Console.WriteLine("Ya existe un lector con este DNI.")
+                return false;
+            }
+
+            Lector nuevoLector = new Lector(dni, nombre);
+            lectores.Add(nuevoLector);
+            Console.WriteLine($"Lector {nombre} agregado exitosamente.");
+            return true;
+        }
+        public string prestarLibro(string titulo, string dni)
+        {
+            if (dni == null)
+            {
+                Console.WriteLine("LECTOR INEXISTENTE");
+            }
+            if (titulo == null)
+            {
+                Console.WriteLine("LIBRO INEXISTENTE");
+            }
         }
     }
 }
